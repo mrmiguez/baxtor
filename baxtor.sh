f25ed8a72121f6b0dbb310517476fd063db58006
mrmiguez <r.m.miguez@gmail.com>
2015-01-07 11:33:19 -0500

    variables added for user input

--- baxtor.sh
+++ baxtor.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-echo "Enter a target element: "
+echo "Enter a search target:"
 read target
-echo "Enter XPath for inserted element: "
+echo "Enter XPath for inserted element:"
 read xpath
-for f in $( grep -L digitalOrigin ./*.xml | cut -c 3- ); do
-	xmlstarlet ed -L -s //_:physicalDescription -t elem -n digitalOrigin -v "digitized other analog" $f 
+echo "What is the name of your inserted element?"
+read element
+echo "What value goes into your new element?"
+read value
+for f in $( grep -L $target ./*.xml | cut -c 3- ); do
+	xmlstarlet ed -L -s $xpath -t elem -n $element -v "$value" $f 
 	echo $f "modified"
 done
